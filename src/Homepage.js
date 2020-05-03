7bc8173c8b4f857b6eb71821901b34f4bda541b2
Addy2012 <kumaranand287@gmail.com>
2020-05-03 19:14:44 +0530

    commit for npm eject

---
+++ src/Homepage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './homepage.module.sass';
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="direectory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title"> Broom </h1>
+                        <span className="subtitle"> buy now </span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title"> Broom </h1>
+                        <span className="subtitle"> buy now </span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title"> Broom </h1>
+                        <span className="subtitle"> buy now </span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title"> Broom </h1>
+                        <span className="subtitle"> buy now </span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title"> Broom </h1>
+                        <span className="subtitle"> buy now </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
